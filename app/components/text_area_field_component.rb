03155a122be0d62f0107b8a811e153448e06bea3
wailkouicemm <kw_kouicem@esi.dz>
2026-04-20 15:56:23 +0200

    Move default line clamp value from CSS to component

--- app/components/text_area_field_component.rb
+++ app/components/text_area_field_component.rb
@@ -3,7 +3,7 @@
 class TextAreaFieldComponent < ViewComponent::Base
   include InternationalisationHelper
 
-  def initialize(value: , see_more_text: t('components.see_more') , see_less_text: t('components.see_less'), lines: nil)
+  def initialize(value: , see_more_text: t('components.see_more') , see_less_text: t('components.see_less'), lines: 5)
     super
     @value = value
     @see_more_text = see_more_text
