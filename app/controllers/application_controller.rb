cbbaf13bf5e2525d2ffc74e71c81ef8b752ad3a8
Amine BOUKERFA <boukerfa.ma@gmail.com>
2026-07-21 11:44:08 +0200

    feat: expose fairness_url in /site_config (#1270)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -169,7 +169,8 @@ class ApplicationController < ActionController::Base
         ncbo_annotator_url: $NCBO_ANNOTATOR_URL,
         ncbo_apikey: $NCBO_API_KEY,
         interportal_hash: $INTERPORTAL_HASH,
-        resolve_namespace: RESOLVE_NAMESPACE
+        resolve_namespace: RESOLVE_NAMESPACE,
+        fairness_url: $FAIRNESS_URL
     }
     config[:ncbo_slice] = @subdomain_filter[:acronym] if (@subdomain_filter[:active] && !@subdomain_filter[:acronym].empty?)
     config.to_json
